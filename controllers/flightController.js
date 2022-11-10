cddd8ae218adccaa0b8926606889994efa21d6b5
bhaybhiisparks <bhaybhiisparks@gmail.com>
2022-11-10 18:51:52 +0100

    refractored the controller and model files

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -2,23 +2,36 @@ let flights = require("../models/Flight");
 const { v4: uuidv4 } = require('uuid');
 
 exports.all_flights = (req, res) =>{
-    res.send(flights);
+    res.json(flights);
 }
 
 exports.single_flight = (req, res) =>{
     const id = req.params.id;
-    const singleFlight = flights.find((flight) => flight.id == id);
+    const singleFlight = flights.find((flight) => flight.id === id);
 
-    res.send(singleFlight);
+
+    if(!singleFlight){
+        res.send("No flights with that ID found. Please check the ID number and try again");
+    }else{
+        res.json(singleFlight);
+    }
 }
 
 exports.add_flight = (req, res) =>{
+   
     let flight = req.body;
     let Id = uuidv4();
     const flightId = {...flight, id:Id};
 
-
-    flights.push(flightId);
+    if (typeof title === "string" && typeof time === "string" && typeof price === "number" && typeof date === "string") {
+        flights.push(flightId);
+    }else{
+        res.json({
+            "message": "bad request",
+            "details" : "invalid input"
+        });
+    }
+    
 
     res.send(`Added ${flight.title}, ${flight.time}, ${flight.price}, ${flight.date}`);
 }
@@ -27,12 +40,12 @@ exports.edit_flight = (req, res) =>{
     const id = req.params.id;
     const {title, time, price, date} = req.body;
 
-    const flight = flights.find((flight) => flight.id == id);
+    const flight = flights.find((flight) => flight.id === id);
 
-    if (title) flight.title = title;
-    if (title) flight.time = time;
-    if (title) flight.price = price;
-    if (title) flight.date = date;
+    if (title) { flight.title = title; }
+    if (time) { flight.time = time; }
+    if (price) { flight.price = price; }
+    if (date) { flight.date = date; }
 
     res.send(`Flight with id : ${id} has been updated`)
 }
--- models/Flight.js
+++ models/Flight.js
@@ -1,3 +1,10 @@
-exampleModel = [];
+exampleModel = [
+    {
+        title: "flight to canada",
+        time: "1pm",
+        price: 26000,
+        date: "26-06-2022"
+        }
+];
 
 module.exports = exampleModel;
